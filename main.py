5cc18c9dd77475e93ecb9b2562d2839ec514eb98
Ota Novotny <otas.novotny@gmail.com>
2024-10-26 11:41:37 +0200

    Cleanup

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 from src.Replicator import Replicator
 from my_logging import getMyLogger
 import argparse, logging
-import os
 
 logger = getMyLogger(__name__)
 
-
-def add_logging_file_handler(filename: str) -> None:
-  file_handler = logging.FileHandler(args.logFilename)
-  file_handler.setFormatter(logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s'))
-  file_handler.setLevel(logging.WARNING)
-  logger.addHandler(file_handler)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="Replicator argument parser.")
@@ -19,7 +12,6 @@ if __name__ == '__main__':
   parser.add_argument('--logFilename', nargs='?', default='replicator.log', help='Log filename')
 
   args = parser.parse_args()
-  add_logging_file_handler(args.logFilename)
 
   replicator = Replicator(source_dir=args.sourceDir, replica_dir=args.replicaDir, log_filename=args.logFilename)
   replicator.sync()
